a5be5d486df95763b3b5a19b62ee2c6149301889
RakeeshKumar <rakeeshkumar.r95@gmail.com>
2021-08-14 08:40:25 +1000

    Added the example2 algorithm

---
+++ Example2.py
@@ -0,0 +1,15 @@
+# CREATING A EMPLOYEE DATABASE - INITIALISE A EMPLOYEE DICTIONARY
+# GET THE NUMBER OF EMPLOYEES TO BE ADDED TO DATABASE
+# GET THE EMPLOYEE ID FROM USER - eg E1
+# GET THE EMPLOYEE FIRST NAME FROM USER - eg MOHAN
+# GET THE EMPLOYEE LAST NAME FROM USER - eg BABU
+# CONCATENATE BOTH FIRST AND LAST NAME as FULL NAME - eg MOHAN BABU ( user join or '+' to join the string)
+# ASK FOR OPTIONS 1,2,3
+# 1 FOR FULL LIST OF EMPLOYEE
+#  eg:{"E1": "MOHAN BABU", "E2":"SARAH MOHAN"} - output must be similar to this
+# 2 SEARCH THE EMPLOYEE BASED ON EMPLOYEE ID
+#    i) get the employee id and give the employee full name - for E1, "MOHAN BABU" must be returned
+# 3 EXIT
+#    break and come out of look
+# if any other options, give the message as wrong request and break out.
+
